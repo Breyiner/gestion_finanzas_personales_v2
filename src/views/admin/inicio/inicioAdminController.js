4f28654fb61536b98183378f56bd588c27af470c
Breyiner <breyneracosta7@gmail.com>
2025-08-03 22:17:34 -0500

    se muestra contenido en la vista de inicio del administrador

--- src/views/admin/inicio/inicioAdminController.js
+++ src/views/admin/inicio/inicioAdminController.js
@@ -1,3 +1,35 @@
-export const inicioAdminController = () => {
+import { animateValue } from "../../../helpers/animacionValores";
+import { get } from "../../../helpers/api"
+
+export const inicioAdminController = async () => {
+
+    // Cantidad de Usuarios
+    let countUsersHTML = document.querySelector('.admin-info-card__valor--usuarios');
+    let countUsers = (await get('usuarios/cantidad')).data;
+
+    animateValue(countUsersHTML, countUsers.cantidad, false);
+    
+
+    // Cantidad de Movimientos
+    let countMovsHTML = document.querySelector('.admin-info-card__valor--movimientos');
+    let countMovs = (await get('movimientos/cantidad')).data;
+
+    animateValue(countMovsHTML, countMovs.cantidad, false);
+    
+
+    // Cantidad de Metas
+    let countMetasHTML = document.querySelector('.admin-info-card__valor--metas');
+    let countMetas = (await get('metas/cantidad')).data;
+
+    animateValue(countMetasHTML, countMetas.cantidad, false);
+    
+
+    // Cantidad de Aportes
+    let countAportesHTML = document.querySelector('.admin-info-card__valor--aportes');
+    let countAportes = (await get('aportes/cantidad')).data;
+
+    animateValue(countAportesHTML, countAportes.cantidad, false);
+    
 
 }
+
