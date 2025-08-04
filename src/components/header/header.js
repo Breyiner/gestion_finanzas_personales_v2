a94e0dfa993b40444d81825bdab442c5fe469d4e
Breyiner <breyneracosta7@gmail.com>
2025-08-03 22:11:24 -0500

    se modifican los header para mostrar la informacion correspondiente

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,32 @@
 import headerHtml from  './header.html?raw';
 
 
+
 export const renderHeader = (elememto) => {
     elememto.innerHTML = headerHtml
     
+    cargarContenido();
 }
+
+function cargarContenido() {
+
+    const nombreCompleto = `${localStorage.getItem('nombre')} ${localStorage.getItem('apellido')}`;
+
+    let usernameHeader = document.querySelector('.profile__name');
+    let usernameMenu = document.querySelector('.profile-menu__username');
+    usernameHeader.textContent = nombreCompleto;
+    usernameMenu.textContent = nombreCompleto;
+}
+
+function logout() {
+
+    localStorage.clear();
+    window.location.href = '#/login';
+
+}
+
+document.addEventListener('click', (e) => {
+
+    if(e.target.closest('#logout')) logout();
+
+})
--- src/components/headerAdmin/headerAdmin.js
+++ src/components/headerAdmin/headerAdmin.js
@@ -1,7 +1,32 @@
+import { confirmModal } from '../modales/modalConfirm';
 import headerHtml from  './headerAdmin.html?raw';
 
 
 export const renderHeaderAdmin = (elememto) => {
     elememto.innerHTML = headerHtml
     
+    cargarContenido();
 }
+
+function cargarContenido() {
+
+    const nombreCompleto = `${localStorage.getItem('nombre')} ${localStorage.getItem('apellido')}`;
+
+    let usernameHeader = document.querySelector('.profile__name');
+    let usernameMenu = document.querySelector('.profile-menu__username');
+    usernameHeader.textContent = nombreCompleto;
+    usernameMenu.textContent = nombreCompleto;
+}
+
+function logout() {
+
+    localStorage.clear();
+    window.location.href = '#/login';
+
+}
+
+document.addEventListener('click', (e) => {
+
+    if(e.target.closest('#logout')) logout();
+
+})
