3309fa20435f5979c973c46df905dbb79a1a5d86
Breyiner <breyneracosta7@gmail.com>
2025-08-03 22:19:30 -0500

    se agrega informacion a la vista de usuarios y se incorporan los respectivos modales

---
+++ src/components/modales/crearUsuarios.js
@@ -0,0 +1,133 @@
+import { error, success } from '../../helpers/alertas';
+import { get, post } from '../../helpers/api';
+import { cerrarTodos, mostrarModal } from '../../helpers/modalManagement';
+import { datos, validarCampo, validarCampos, validarCorreo, validarMaximo, validarPassword, validarTexto } from '../../helpers/validaciones';
+import htmlContent from  './crearUsuario.html?raw';
+
+let funcControlador = null;
+
+export const abrirModalNewUser = async (controlador) => {
+
+    funcControlador = controlador;
+
+    mostrarModal(htmlContent);
+
+    await configurarFormulario();
+
+}
+
+async function configurarFormulario() {
+
+    await llenarGeneros();
+    await llenarCiudades();
+    gestionarValidaciones();
+    
+}
+
+async function llenarCiudades() {
+
+    let {data} = await get('ciudades');
+    let selectCiudades = document.querySelector('#ciudad');
+
+    selectCiudades.innerHTML = '<option value="">Seleccione...</option>';
+    data.forEach(({id, nombre}) => {
+       
+        selectCiudades.innerHTML += `<option value="${id}">${nombre}</option>`;
+        
+    });
+
+}
+
+async function llenarGeneros() {
+
+    let {data} = await get('generos');
+    let selectGeneros = document.querySelector('#genero');
+
+    selectGeneros.innerHTML = '<option value="">Seleccione...</option>';
+    data.forEach(({id, nombre}) => {
+       
+        selectGeneros.innerHTML += `<option value="${id}">${nombre}</option>`;
+        
+    });
+}
+
+function gestionarValidaciones() {
+
+    let nombre = document.querySelector("#nombre");
+    let apellido = document.querySelector("#apellido");
+    let ciudades = document.querySelector('#ciudad');
+    let generos = document.querySelector('#genero');
+    let correo = document.querySelector("#correo");
+    let contrasena = document.querySelector("#contrasena");
+    
+    nombre.addEventListener('keydown', (e) => {
+        validarTexto(e);
+        validarMaximo(e, 30);
+    })
+    
+    apellido.addEventListener('keydown', (e) => {
+        validarTexto(e);
+        validarMaximo(e, 30);
+    })
+
+    correo.addEventListener('keydown', (e) => {
+        validarMaximo(e, 30);
+    })
+
+    contrasena.addEventListener('keydown', (e) => {
+        validarMaximo(e, 20);
+    })
+
+    nombre.addEventListener('blur', (e) => validarCampo(e));
+    
+    apellido.addEventListener('blur', (e) => validarCampo(e));
+
+    ciudades.addEventListener('blur', (e) => validarCampo(e));
+    
+    generos.addEventListener('blur', (e) => validarCampo(e));
+
+    correo.addEventListener('blur', (e) => {
+        validarCampo(e);
+        validarCorreo(e);
+    });
+
+    contrasena.addEventListener('blur', (e) => {
+        validarCampo(e);
+        validarPassword(e);
+    });
+}
+
+async function validarSubmit(e) {
+
+    if(validarCampos(e)) {
+        
+        console.log(datos);
+        let botonCrear = document.querySelector(('#newUser'));
+        let textoOriginal = botonCrear.textContent;
+        botonCrear.disabled = true;
+        botonCrear.textContent = "cargando...";
+        
+        let response = await post(datos, 'usuarios/register');
+        
+        botonCrear.textContent = textoOriginal;
+        botonCrear.disabled = false;
+
+        cerrarTodos();
+        if(response.success) {
+            let confirmacion = success(response.message);
+            e.target.reset();   
+            if((await confirmacion).isConfirmed) funcControlador();
+        }
+        else {
+            error(response.message);
+        }
+    }
+
+}
+
+document.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    if(e.target.classList.contains('form-newUser')) await validarSubmit(e);
+    
+})
--- src/views/admin/usuarios/usuariosController.js
+++ src/views/admin/usuarios/usuariosController.js
@@ -1,15 +1,212 @@
-import modalCrear from  '../../../components/modales/crearUsuario.html?raw';
-import modalEditar from  '../../../components/modales/editarUsuario.html?raw';
+import { abrirModalNewUser } from '../../../components/modales/crearUsuarios';
+import { abrirModalEditUser } from '../../../components/modales/editarUsuario';
+import { confirmModal } from '../../../components/modales/modalConfirm';
+import { error, success } from '../../../helpers/alertas';
+import { delet, get } from '../../../helpers/api';
 import { cerrarModal, mostrarModal } from '../../../helpers/modalManagement';
 
-export const usuariosController = () => {
+const user_id = localStorage.getItem('usuario_id');
+
+export const usuariosController = async () => {
     
+    let containerTable = document.querySelector('.container-table');
+    let {data} = await get('usuarios/tabla');
+    console.log(data);
+    
+    containerTable.innerHTML = "";
+    createBtnNew(containerTable);
+    crearTabla(containerTable, data);
+}
+
+function createBtnNew(container) {
+
+    const btnNuevo = document.createElement('button');
+    btnNuevo.classList.add('boton', 'boton--verde');
+    btnNuevo.setAttribute('type', 'button');
+    btnNuevo.setAttribute('id', 'crearUsuario');
+    btnNuevo.textContent = "Nuevo Usuario";
+
+    container.append(btnNuevo);
+
+}
+
+function crearTabla(container, data) {
+
+    if (data.length == 0) {
+        let sinRegistros = document.createElement('span');
+        sinRegistros.textContent = "No hay registros";
+        sinRegistros.classList.add('elemento-vacio');
+        container.append(sinRegistros);
+        return;
+    }
+
+
+    // creamos los elementos
+    const table = document.createElement('table');
+    const tableHeader = document.createElement('thead');
+    const rowHeader = document.createElement('tr');
+    const cellId = document.createElement('th');
+    const cellRol = document.createElement('th');
+    const cellNombre = document.createElement('th');
+    const cellApellido = document.createElement('th');
+    const cellCorreo = document.createElement('th');
+    const cellContrasena = document.createElement('th');
+    const cellGenero = document.createElement('th');
+    const cellCiudad = document.createElement('th');
+    const cellEstado = document.createElement('th');
+    const cellEditar = document.createElement('th');
+    const cellEliminar = document.createElement('th');
+
+    // dar clases a los elementos
+    table.classList.add('tabla');
+    tableHeader.classList.add('tabla__header');
+    cellId.classList.add('tabla__celda', 'tabla__celda--header');
+    cellRol.classList.add('tabla__celda', 'tabla__celda--header');
+    cellNombre.classList.add('tabla__celda', 'tabla__celda--header');
+    cellApellido.classList.add('tabla__celda', 'tabla__celda--header');
+    cellCorreo.classList.add('tabla__celda', 'tabla__celda--header');
+    cellContrasena.classList.add('tabla__celda', 'tabla__celda--header');
+    cellGenero.classList.add('tabla__celda', 'tabla__celda--header');
+    cellCiudad.classList.add('tabla__celda', 'tabla__celda--header');
+    cellEstado.classList.add('tabla__celda', 'tabla__celda--header');
+    cellEditar.classList.add('tabla__celda', 'tabla__celda--header');
+    cellEliminar.classList.add('tabla__celda', 'tabla__celda--header');
+
+    // dar contenido
+    cellId.textContent = 'ID';
+    cellRol.textContent = 'Rol';
+    cellNombre.textContent = 'Nombre';
+    cellApellido.textContent = 'Apellido';
+    cellCorreo.textContent = 'Correo';
+    cellContrasena.textContent = 'Contrasena';
+    cellGenero.textContent = 'Género';
+    cellCiudad.textContent = 'Ciudad';
+    cellEstado.textContent = 'Estado';
+    cellEditar.textContent = 'Editar';
+    cellEliminar.textContent = 'Eliminar';
+
+    const tableBody = document.createElement('tbody');
+
+    data.forEach(({id, rol, nombre, apellido, correo, genero, ciudad, estado}) => {
+        
+        // Crear cuerpo de la tabla
+        const rowBody = document.createElement('tr');
+        const cellIdBody = document.createElement('td');
+        const cellRoleBody = document.createElement('td');
+        const cellNameBody = document.createElement('td');
+        const cellLstNameBody = document.createElement('td');
+        const cellEmailBody = document.createElement('td');
+        const cellPswdBody = document.createElement('td');
+        const cellGenderBody = document.createElement('td');
+        const cellCityBody = document.createElement('td');
+        const cellEstateBody = document.createElement('td');
+        const cellEditBody = document.createElement('td');
+        const cellDeleteBody = document.createElement('td');
+    
+        //dar clases y atributos
+        rowBody.setAttribute('data-userId', id);
+        rowBody.classList.add('tabla__fila');
+        cellIdBody.classList.add('tabla__celda');
+        cellRoleBody.classList.add('tabla__celda');
+        cellNameBody.classList.add('tabla__celda');
+        cellLstNameBody.classList.add('tabla__celda');
+        cellEmailBody.classList.add('tabla__celda');
+        cellPswdBody.classList.add('tabla__celda');
+        cellGenderBody.classList.add('tabla__celda');
+        cellCityBody.classList.add('tabla__celda');
+        cellEstateBody.classList.add('tabla__celda');
+        cellEditBody.classList.add('tabla__celda');
+        cellDeleteBody.classList.add('tabla__celda');
+    
+        //contenido celdas cuerpo
+        cellIdBody.textContent = id;
+        cellRoleBody.textContent = rol;
+        cellNameBody.textContent = nombre;
+        cellLstNameBody.textContent = apellido;
+        cellEmailBody.textContent = correo;
+        cellPswdBody.textContent = '******';
+        cellGenderBody.textContent = genero;
+        cellCityBody.textContent = ciudad;
+        cellEstateBody.textContent = estado;
+    
+        // crear botones
+        const btnEditar = document.createElement('button');
+        const btnEliminar = document.createElement('button');
+    
+        //dar clases y atributos
+        btnEditar.classList.add('boton', 'boton--azul');
+        btnEliminar.classList.add('boton', 'boton--rojo');
+        btnEditar.setAttribute('data-userId', id);
+        btnEditar.setAttribute('id', 'editarUsuario');
+        btnEliminar.setAttribute('type', 'button');
+        btnEliminar.setAttribute('id', 'eliminarUsuario');
+        btnEliminar.setAttribute('data-userId', id);
+
+        if(user_id == id) {
+            
+            btnEditar.disabled = true;
+            btnEditar.classList.remove('boton--azul');
+        }
+
+        if(user_id == id || estado == "Inactivo") {
+            btnEliminar.disabled = true;
+            btnEliminar.classList.remove('boton--rojo');
+        }
+    
+        // dar contenido a los botones
+        btnEditar.textContent = "Editar";
+        btnEliminar.textContent = "Eliminar";
+    
+        cellEditBody.append(btnEditar);
+        cellDeleteBody.append(btnEliminar);
+        
+        rowBody.append(cellIdBody, cellRoleBody, cellNameBody, cellLstNameBody, cellEmailBody, cellPswdBody, cellGenderBody, cellCityBody, cellEstateBody, cellEditBody, cellDeleteBody);
+        tableBody.append(rowBody);
+    });
+
+
+    rowHeader.append(cellId, cellRol, cellNombre, cellApellido, cellCorreo, cellContrasena, cellGenero, cellCiudad, cellEstado, cellEditar, cellEliminar);
+    tableHeader.append(rowHeader);
+
+
+    table.append(tableHeader, tableBody);
+
+    container.append(table);
+}
+
+async function eliminar(boton) {
+    
+    let dataId = boton.dataset.userid;
+    
+
+    const confirmacion = await confirmModal("¿Está seguro de eliminar el usuario?");
+    if(confirmacion){
+        const respuesta = await delet(`usuarios/soft/${dataId}`);
+        
+        if(!respuesta.success){
+            console.log(respuesta);
+            error(respuesta.message);
+            return
+        }
+        
+        await success(respuesta.message)
+        usuariosController();
+    }
+}
+
+async function editarUsuario(boton) {
+
+    let userId = boton.dataset.userid;
+
+    abrirModalEditUser(userId);
+
 }
 
 document.addEventListener('click', async (e) => {
 
-    if (e.target.closest('#crearUsuario')) mostrarModal(modalCrear);
-    if (e.target.closest('.editarUsuario')) mostrarModal(modalEditar);
+    if (e.target.closest('#crearUsuario')) await abrirModalNewUser(usuariosController);
+    if (e.target.closest('#editarUsuario')) await editarUsuario(e.target.closest('#editarUsuario'));
+    if (e.target.closest('#eliminarUsuario')) await eliminar(e.target.closest('#eliminarUsuario'));
 
 
     if (e.target.closest('.modal-exit--usuario')) cerrarModal();
